403be7d6cc997947dd438e66e7bc4e04acffd19d
DariaMirzoyants <10955618@my.uvu.edu>
2021-06-05 18:41:07 -0600

    Added MainPlayer

---
+++ SummerProject/Assets/Scenes/MainPlayer.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainPlayer : MonoBehaviour
+{
+    // Use this for intitialization
+    void Start() 
+    {
+        Debug.Log("I am alive!");
+    }
+
+    // Update is called once per frame
+    void Update()
+        
+    }
+}
