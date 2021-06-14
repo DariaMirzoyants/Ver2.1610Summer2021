3a6dfe1bb10f04e989e3e3bc024dbb064d58a885
DariaMirzoyants <10955618@my.uvu.edu>
2021-06-13 21:39:23 -0600

    Added changes in Variables

---
+++ SummerProject/Assets/VariablesAndOperators/Variables.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+    static void Main(string[] args)
+    {
+        string mname = "Daria";
+        Console.WriteLine(name);
+
+        int myNum = 15;
+        myNum = 20;
+        Console.WriteLine(myNum);
+    }
+  }
+
+
