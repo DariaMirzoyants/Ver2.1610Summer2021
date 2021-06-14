6afc3969869ccbfdf6ae37364efd8aea3484ca95
DariaMirzoyants <10955618@my.uvu.edu>
2021-06-13 22:10:35 -0600

    Added changes in Operators

--- SummerProject/Assets/Scenes/MainPlayer.cs
+++ SummerProject/Assets/Scenes/MainPlayer.cs
@@ -17,4 +17,4 @@ public class MainPlayer : MonoBehaviour
     void Update()
         
     }
-}
+ }
--- SummerProject/Assets/VariablesAndOperators/Operators.cs
+++ SummerProject/Assets/VariablesAndOperators/Operators.cs
@@ -1,18 +1,37 @@
 using System.Collections;
+
 using System.Collections.Generic;
-using UnityEngine;
-
-public class Operators : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   using UnityEngine;
+   {
+     public class Operators : MonoBehaviour
+      {
+        int sum1 = 100 + 50;
+        int sum2 = sum1 + 250;
+        int sum3 = sum2 + sum2;
+        Console.WriteLine(sum1);
+        Console.WriteLine(sum2);
+        Console.WriteLine(sum3);
+        int x = 5;
+        int y = 3;
+        Console.WriteLine(x + y);
+        Console.WriteLine(x - y);
+        Console.WriteLine(x * y);
+        Console.WriteLine(x / y);
+        Console.WriteLine(x % y);
+        x++;
+        Console.WriteLine(x);
+        x--;
+        Console.WriteLine(x);
+        Console.WriteLine(x == y);
+        Console.WriteLine(x != y);
+        Console.WriteLine(x > y);
+        Console.WriteLine(x < y);
+        Console.WriteLine(x >= y);
+        Console.WriteLine(x <= y);
+        Console.WriteLine(x > 3 && x < 10);
+        Console.WriteLine(x > 3 || x < 4);
+        Console.WriteLine(!(x > 3 && x < 10));
+        }
+   }
 }
