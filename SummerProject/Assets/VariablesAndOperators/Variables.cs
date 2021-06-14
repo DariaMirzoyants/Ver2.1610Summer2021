492d1225f6597cd775eb07a025792367d5902dfd
DariaMirzoyants <10955618@my.uvu.edu>
2021-06-13 21:47:14 -0600

    Added changes in Variables

--- SummerProject/Assets/VariablesAndOperators/Variables.cs
+++ SummerProject/Assets/VariablesAndOperators/Variables.cs
@@ -12,7 +12,11 @@ public class Variables : MonoBehaviour
         int myNum = 15;
         myNum = 20;
         Console.WriteLine(myNum);
+        float f1 = 35e3F;
+        double d1 = 12E4D;
+        Console.WriteLine(f1);
+        Console.WriteLine(d1);
     }
-  }
+}
 
 
