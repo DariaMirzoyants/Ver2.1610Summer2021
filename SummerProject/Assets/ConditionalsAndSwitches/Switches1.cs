de4a01b47b697740d8fd174a934973d7c8101b69
DariaMirzoyants <10955618@my.uvu.edu>
2021-06-18 23:30:14 -0600

    Added changes in Switches1

--- SummerProject/Assets/ConditionalsAndSwitches/Switches1.cs
+++ SummerProject/Assets/ConditionalsAndSwitches/Switches1.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Switches1 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    static void Main(string[] args)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        int day = 4;
+        switch (day)
+        {
+            case 6:
+                Console.WriteLine("Today is Saturday.");
+                break;
+            case 7:
+                Console.WriteLine("Today is Sunday.");
+                break;
+            default:
+                Console.WriteLine("Looking forward to the Weekend.");
+                break;
+        }
     }
 }
+
