895eb05ad7edf690389a5e8c64f4e6ed312887e1
DariaMirzoyants <10955618@my.uvu.edu>
2021-06-24 23:49:28 -0600

    Added changes in ForeachLoopsWithArrays

--- SummerProject/Assets/Loops/ForeachLoopsWithArrays.cs
+++ SummerProject/Assets/Loops/ForeachLoopsWithArrays.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class ForeachLoopsWithArrays : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  public class ForeachLoopsWithArrays : MonoBehaviour
+   {
+    static void Main(string[] args)
+     {
+       string[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+       for (int i = 0; i < cars.Length; i++)
+       {
+         Console.WriteLine(cars[i]);
+       }
+     }
+   }
 }
