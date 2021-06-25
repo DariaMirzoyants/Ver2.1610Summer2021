fc06de2ebc667472070fbdebdc99913efe1bef8a
DariaMirzoyants <10955618@my.uvu.edu>
2021-06-24 23:12:33 -0600

    Added changes in WhileLoops

---
+++ SummerProject/Assets/Loops/WhileLoops.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+{
+public class WhileLoops : MonoBehaviour
+{
+    private int i = 0;
+        do
+
+    {
+        Console.WriteLine(i);
+        i++;
+    }
+
+while (i< 5);
+}
+}
+
